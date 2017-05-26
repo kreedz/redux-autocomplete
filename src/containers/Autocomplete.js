3a47a66a027ffb206686da83d37df24472146c24
kreedz <kreedz@users.noreply.github.com>
2017-05-26 18:44:01 +0500

    init bootstrapped

--- src/containers/Autocomplete.js
+++ src/containers/Autocomplete.js
@@ -1,7 +1,63 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Button, Dropdown, MenuItem, FormControl } from 'react-bootstrap'
 
 import { getOptions } from 'actions'
+
+class CustomToggle extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.onChange = e => this.setState({ value: e.target.value });
+
+    this.state = { value: '' };
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+
+    this.props.onClick(e);
+  }
+
+  render() {
+    return (
+      <FormControl
+        ref={c => { this.input = c; }}
+        type="text"
+        placeholder="Type to filter..."
+        onChange={this.onChange}
+        onClick={this.handleClick}
+        value={this.state.value}
+    />
+    );
+  }
+}
+
+class CustomMenu extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.onChange = e => this.setState({ value: e.target.value });
+
+    this.state = { value: '' };
+  }
+
+  render() {
+    const { children } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className="dropdown-menu" style={{ padding: '' }}>
+        <ul className="list-unstyled">
+          {React.Children.toArray(children).filter(child => (
+            !value.trim() || child.props.children.indexOf(value) !== -1
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 class Autocomplete extends React.Component {
   constructor() {
@@ -29,10 +85,22 @@ class Autocomplete extends React.Component {
     );
     return (
       <div>
+        <Button bsStyle="success">asd</Button>
         <input list="data" onChange={::this.filterDataList}/>
         <datalist id="data">
           {options}
         </datalist>
+
+        <Dropdown id="dropdown-custom-menu">
+          <CustomToggle bsRole="toggle" />
+
+          <CustomMenu bsRole="menu">
+            <MenuItem eventKey="1">Red</MenuItem>
+            <MenuItem eventKey="2">Blue</MenuItem>
+            <MenuItem eventKey="3" active>Orange</MenuItem>
+            <MenuItem eventKey="1">Red-Orange</MenuItem>
+          </CustomMenu>
+        </Dropdown>
       </div>
     );
   }
