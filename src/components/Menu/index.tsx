44fc85649b6bc2113d8cf4e4dd798f844f69bc08
kreedz <kreedz@users.noreply.github.com>
2017-06-06 18:10:47 +0500

    Menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import styles from './styles.css';
+
+const Menu: React.SFC<{children?: React.ReactNode}> = ({children}) => (
+  <ul className={`${styles.dropdownMenu} dropdown-menu list-unstyled`}>
+    {children}
+  </ul>
+);
+
+export default Menu;
