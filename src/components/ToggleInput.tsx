9d04beda2594d4b86a9372786f6bbf274d6bd417
kreedz <kreedz@users.noreply.github.com>
2017-06-06 14:29:47 +0500

    ToggleInput

---
+++ src/components/ToggleInput.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FormControl, FormControlProps } from 'react-bootstrap';
+
+interface IProps extends FormControlProps {
+  filterDataList(event: any): void;
+  menuNavigate(event: any): void;
+  getInput(): string;
+}
+
+const ToggleInput: React.StatelessComponent<IProps> = props => (
+  <FormControl
+    type="text"
+    placeholder="Type to filter..."
+    onChange={props.filterDataList}
+    onKeyDown={props.menuNavigate}
+    value={props.getInput()}
+  />
+);
+
+export default ToggleInput;
