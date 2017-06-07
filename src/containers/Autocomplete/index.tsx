7f2728a669f60d4d06578cc249802605c10f42b7
kreedz <kreedz@users.noreply.github.com>
2017-06-07 12:19:43 +0500

    Autocomplete to ts, changes to solve tslint warnings

--- src/containers/Autocomplete/index.tsx
+++ src/containers/Autocomplete/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { getOptions } from 'actions'
-import Menu from 'components/Menu'
-import ToggleInput from 'components/ToggleInput'
+import { getOptions } from 'actions';
+import Menu from 'components/Menu';
+import ToggleInput from 'components/ToggleInput';
 
-import styles from './styles.css'
+import styles from './styles.css';
 
 const KeyCode = {
   UP: 38,
   DOWN: 40,
   RETURN: 13,
-}
+};
 
 class Autocomplete extends React.Component {
   constructor() {
@@ -26,7 +26,7 @@ class Autocomplete extends React.Component {
       isMenuOpen: false,
       input: '',
       selectedItemIndex: -1,
-    }
+    };
   }
   componentDidMount() {
     this.getOptions();
@@ -38,22 +38,24 @@ class Autocomplete extends React.Component {
     }
   }
 
-  onClick(e) {
+  onClick = e => {
     this.setStates({input: e.target.textContent, isItemSelected: true});
   }
   setStates(values, callback) {
     this.setState(prevState => ({...prevState, ...values}), callback);
   }
-  getInput() {
+  getInput = () => {
     return this.state.input;
   }
   getOptions(filterBy = '') {
     const {settingsGettingOptions: {url, field}} = this.state;
     this.props.getOptions(url, field, filterBy);
   }
-  filterDataList(e) {
+  filterDataList = e => {
     const value = e.target.value;
-    this.setStates({input: value, isItemSelected: false, selectedItemIndex: -1});
+    this.setStates({
+      input: value, isItemSelected: false, selectedItemIndex: -1
+    });
     this.getOptions(value);
   }
   getNextSelectedItemIndex() {
@@ -62,7 +64,7 @@ class Autocomplete extends React.Component {
     if (itemsLength > 1 && index < itemsLength - 1) {
         return index + 1;
     }
-    if (index == itemsLength - 1 || index == -1) {
+    if (index === itemsLength - 1 || index === -1) {
         return 0;
     }
   }
@@ -72,7 +74,7 @@ class Autocomplete extends React.Component {
     if (itemsLength > 1 && index > 0) {
       return index - 1;
     }
-    if (index == 0 || index == -1) {
+    if (index === 0 || index === -1) {
       return itemsLength - 1;
     }
   }
@@ -87,11 +89,11 @@ class Autocomplete extends React.Component {
       ul.scrollTop += liCoord.bottom - ulCoord.bottom;
     }
   }
-  menuNavigate(e) {
+  menuNavigate = e => {
     if (!e.altKey || !this.state.isMenuOpen) {
       return;
     }
-    switch(e.keyCode) {
+    switch (e.keyCode) {
       case KeyCode.UP:
         this.setStates(
           {
@@ -117,34 +119,40 @@ class Autocomplete extends React.Component {
   }
   isMenuHasToOpen() {
     const value = this.state.input;
-    return (this.props.options.isFetching || this.state.isItemSelected) ? false :
-      this.props.options.items.length && value.length > 0
+    return (this.props.options.isFetching || this.state.isItemSelected) ?
+      false : this.props.options.items.length && value.length > 0;
   }
-  onItemHover(e) {
+  onItemHover = e => {
     const value = e.target.value;
-    if (e.type == 'mouseenter') {
+    if (e.type === 'mouseenter') {
       this.setStates({selectedItemIndex: value});
     } else if (e.type === 'mouseleave') {
       this.setStates({selectedItemIndex: -1});
     }
   }
   render() {
-    const menu = this.props.options.items.map((item, index) =>
-      <li onMouseEnter={::this.onItemHover} onMouseLeave={::this.onItemHover}
-        onClick={::this.onClick}
-        key={index} value={index}
+    const setMenuItem = index => e => this._menuItemNodes.set(index, e);
+    const menu = this.props.options.items.map((item, index) => (
+      <li
+        onMouseEnter={this.onItemHover}
+        onMouseLeave={this.onItemHover}
+        onClick={this.onClick}
+        key={index}
+        value={index}
         className={index === this.state.selectedItemIndex ? styles.onHover : ''}
-        ref={e => this._menuItemNodes.set(index, e)}
+        ref={setMenuItem(index)}
       >
         {item}
       </li>
-    );
+    ));
     const open = this.state.isMenuOpen ? 'open' : '';
     return (
       <div>
         <div className={`dropdown ${open}`}>
-          <ToggleInput filterDataList={::this.filterDataList}
-            getInput={::this.getInput} menuNavigate={::this.menuNavigate}
+          <ToggleInput
+            filterDataList={this.filterDataList}
+            getInput={this.getInput}
+            menuNavigate={this.menuNavigate}
           />
           <Menu>
             {menu}
@@ -156,4 +164,4 @@ class Autocomplete extends React.Component {
 }
 
 const mapStateToProps = ({options}) => ({options});
-export default connect(mapStateToProps, {getOptions})(Autocomplete)
+export default connect(mapStateToProps, {getOptions})(Autocomplete);
