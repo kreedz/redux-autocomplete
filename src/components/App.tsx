834cc054982871af5225ef26333089d3868f8c6b
kreedz <kreedz@users.noreply.github.com>
2017-06-08 14:00:43 +0500

    style changes, remove scroll

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ import Autocomplete from 'containers/Autocomplete';
 const App: SFC<any> = () => (
   <Grid>
     <Row className="show-grid">
-      <Col xs={6} xsOffset={4}>
+      <Col xs={4} xsOffset={4}>
         <Autocomplete/>
       </Col>
     </Row>
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,9 +3,12 @@ import React from 'react';
 import styles from './styles.css';
 
 const Menu: React.SFC<{children?: React.ReactNode}> = ({children}) => (
-  <ul className={`${styles.dropdownMenu} dropdown-menu list-unstyled`}>
-    {children}
-  </ul>
+  <div className={`${styles.dropdownMenu} dropdown-menu`}>
+    <ul className={`list-unstyled`}>
+      {children}
+    </ul>
+    <div className={`${styles.footer}`} />
+  </div>
 );
 
 export default Menu;
