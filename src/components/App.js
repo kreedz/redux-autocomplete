49fb2bfa244d58022a7854d9d6b0ad91339f4bd9
kreedz <kreedz@users.noreply.github.com>
2017-06-18 20:32:13 +0500

    simple version of autocomplete on one file

--- src/components/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Autocomplete from 'containers/Autocomplete';
-
-const App = () => <Autocomplete />;
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,78 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from 'components/App'
-import store from 'store'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+
+// actions
+const getOptions = (url, field) => dispatch =>
+  axios.get(url)
+    .then(response => dispatch(getOptionsSuccess(response, field)));
+
+const getOptionsSuccess = (response, field) => ({
+  type: 'GET_OPTIONS_SUCCESS',
+  payload: response,
+  field,
+});
+
+const setFilterOptions = byName => ({
+  type: 'SET_FILTER',
+  payload: byName,
+});
+
+// reducer
+const options = (state={options:[], filter:''}, action) => {
+  switch(action.type) {
+    case 'GET_OPTIONS_SUCCESS':
+      console.log(action.payload);
+      return {
+        ...state, options: action.payload.data.map(data => data[action.field])
+      };
+    case 'SET_FILTER':
+      return {
+        ...state, filter: action.payload
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(options, composeWithDevTools(applyMiddleware(thunk)));
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: 'https://jsonplaceholder.typicode.com/users',
+      field: 'name',
+    };
+  }
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate());
+    store.dispatch(getOptions(this.state.url, this.state.field));
+  }
+  filterList = (e) => {
+    store.dispatch(setFilterOptions(e.target.value));
+  }
+  render() {
+    const filter = store.getState().filter;
+    const options = store.getState().options
+      .filter(option => option.startsWith(filter))
+      .map((option, key) => <option value={option} key={key} />);
+    console.log(options);
+    return (
+      <div>
+        <input list="data" onChange={this.filterList}/>
+        <datalist id="data">
+          {options}
+        </datalist>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
